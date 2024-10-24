035f1e59f0d0db1aec1b69836f6ac134afece9fc
arjunkdot <81557653+ArjunCybro@users.noreply.github.com>
2024-10-24 22:27:14 +0530

    feat: Product of Array Exception

---
+++ medium/10-product-of-array-except-self/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  const answer = [];
+  let prefix = 1;
+  let postfix = 1;
+
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  for (let i = nums.length - 1; i >= 0; i--) {
+    answer[i] *= postfix;
+    postfix *= nums[i];
+  }
+
+  return answer;
+};
