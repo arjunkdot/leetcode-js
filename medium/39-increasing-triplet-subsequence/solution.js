593ddc2863650f13b797c4ad247645b3e2492529
arjunkdot <arjunkdot@gmail.com>
2025-03-27 21:13:41 +0530

    feat: increasing triplet subsequence

---
+++ medium/39-increasing-triplet-subsequence/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    if(nums.length < 3) return false
+
+    let first = Infinity
+    let second = Infinity
+
+    for(const num of nums) {
+        if(num <= first) {
+            first = num
+        } else if(num <= second) {
+            second = num
+        } else {
+            return true
+        }
+    }
+
+    return false
+};
